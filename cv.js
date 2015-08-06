95c1742e23605108eb6bd4e8c4981cac21c3b7e7
Nais Alcaraz <nsalcaraz@gmail.com>
2015-08-06 15:58:33 +0200

    form modif

--- cv.js
+++ cv.js
@@ -1,7 +1,20 @@
 $(function() {
-	$("#button-send-form").click(function( event ) {
-	  $("#form-contact").submit(function(event) {
-	  	event.preventDefault();
+	$("#button-send-form").click(function(e) {
+		$("#form-contact").submit();
+	});
 
+	$("#form-contact").submit(function(e) {
+	  	e.preventDefault();
+
+		$.ajax({
+	        url: "//formspree.io/nsalcaraz@gmail.com", 
+	        method: "POST",
+	        data: $(this).serialize(),
+	        dataType: "json",
+	        success: function(data){
+	        	$("#form-modal").empty();
+	        	$("#form-modal").html("Merci !");
+	        }
+		});
 	});
 });
