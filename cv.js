23f0093488436b4067b3e55aa15490c7c83b2b1d
Nais Alcaraz <nsalcaraz@gmail.com>
2015-08-06 15:04:54 +0200

    change form

--- cv.js
+++ cv.js
@@ -2,8 +2,6 @@ $(function() {
 	$("#button-send-form").click(function( event ) {
 	  $("#form-contact").submit(function(event) {
 	  	event.preventDefault();
-	  });
-	});
 
 	$.ajax({
         url: "//formspree.io/nalcaraz@gmail.com", 
@@ -15,5 +13,5 @@ $(function() {
           $('#contact_container').html("<h3>Thank You</h3><p>Your request has been sent and I will get in touch with you soon.");
         }
       });
+	});
 });
-
