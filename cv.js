501951292d371e6c586a786f8d49b5c4f5210197
Nais Alcaraz <nsalcaraz@gmail.com>
2015-08-08 16:01:22 +0200

    thanksform modif

--- cv.js
+++ cv.js
@@ -13,7 +13,7 @@ $(function() {
 	        dataType: "json",
 	        success: function(data){
 	        	$("#form-modal").empty();
-	        	$("#form-modal").html("Merci ! <br/><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Fermer</button>");
+	        	$("#form-modal").html("<div style=\"padding: 10px;\">Merci !<br/><br/><button type=\"button\" class=\"btn btn-default\" data-dismiss=\"modal\">Fermer</button></div>");
 	        }
 		});
 	});
